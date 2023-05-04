993875150918e67e876ee1dd8e2bfc74e4458894
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-05-04 07:15:58 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import { robots } from './robots';
 import 'tachyons';
